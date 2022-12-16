21d53fa0cb26de68118e301bdb0930fc371143ce
Abdulmumin <lanreywaju97@gmail.com>
2022-12-16 23:27:28 +0100

    Added base and home templates with tests for the homepage

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 # Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def setUp(self) -> None:
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_renders_the_correct_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+        self.assertNotContains(self.response, "This is not supposed to be here!")
+
+    def test_homepage_url_resolves_homepage_view(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 # Homepage response view
-def home_page_view(request):
-    return HttpResponse("Hello again!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
